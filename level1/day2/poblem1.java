f2cac65ce45d1ca268cffbeb4938fde6377121a4
Shubham Singh <shubhamsingh290596@gmail.com>
2022-09-11 23:34:02 +0530

    Add problem number

--- level1/day2/poblem1.java
+++ level1/day2/poblem1.java
@@ -1,3 +1,4 @@
+//205. Isomorphic Strings
 import java.util.Dictionary;
 import java.util.Hashtable;
 
@@ -9,12 +10,17 @@ class Solution {
         //check length
         if(S.length != T.length) return false;
 
-        Dictionary dic = new Hashtable<>();
+        Dictionary dicS = new Hashtable<>();
+        Dictionary dicT = new Hashtable<>();
         for (int i = 0; i < S.length; i++) {
-            if(dic.get(S[i]) == null){
-                dic.put(S[i], T[i]);
-            }else if(dic.get(S[i]) != T[i]) {
-                return false
+            if(dicS.get(S[i]) == null){
+                dicS.put(S[i], T[i]);
+            }
+            if(dicT.get(T[i]) == null){
+                dicT.put(T[i], S[i]);
+            }
+            if(dicS.get(S[i]).toString().charAt(0) != T[i] || dicT.get(T[i]).toString().charAt(0) != S[i]) {
+                return false;
             }
         }
         return true;
